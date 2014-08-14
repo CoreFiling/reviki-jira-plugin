ab7ec4531bca3480ce28a0240aabcd8314f12122
Michael Walker <mike@barrucadu.co.uk>
2014-08-14 14:11:04 +0100

    This is a JIRA rendering plugin

---
+++ src/main/java/net/hillsdon/reviki/jira/renderer/JiraRevikiRenderingPlugin.java
@@ -0,0 +1,54 @@
+package net.hillsdon.reviki.jira.renderer;
+
+import com.atlassian.jira.issue.RendererManager;
+import com.atlassian.jira.issue.fields.renderer.IssueRenderContext;
+import com.atlassian.jira.issue.fields.renderer.JiraRendererPlugin;
+import com.atlassian.jira.plugin.renderer.JiraRendererModuleDescriptor;
+
+public class JiraRevikiRenderingPlugin implements JiraRendererPlugin {
+  public static final String RENDERER_TYPE = "atlassian-jira-reviki-renderer";
+
+  private JiraRendererModuleDescriptor _jiraRendererModuleDescriptor;
+
+  private final RendererManager _rendererManager;
+
+  public JiraRevikiRenderingPlugin(final RendererManager rendererManager)
+  {
+      _rendererManager = rendererManager;
+  }
+
+  @Override
+  public JiraRendererModuleDescriptor getDescriptor() {
+    return _jiraRendererModuleDescriptor;
+  }
+
+  @Override
+  public String getRendererType() {
+    return RENDERER_TYPE;
+  }
+
+  @Override
+  public void init(final JiraRendererModuleDescriptor jiraRMD) {
+    _jiraRendererModuleDescriptor = jiraRMD;
+  }
+
+  @Override
+  public String render(final String text, final IssueRenderContext ctx) {
+    throw new UnsupportedOperationException("Not yet implemented.");
+  }
+
+  @Override
+  public String renderAsText(final String text, final IssueRenderContext ctx) {
+    return text;
+  }
+
+  @Override
+  public Object transformForEdit(final Object rawValue) {
+    return rawValue;
+  }
+
+  @Override
+  public Object transformFromEdit(final Object editValue) {
+    return editValue;
+  }
+}
--- src/main/java/net/hillsdon/reviki/jira/renderer/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package net.hillsdon.reviki.jira.renderer;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
