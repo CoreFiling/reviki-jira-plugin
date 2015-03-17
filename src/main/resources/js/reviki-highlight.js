6d1577f1e672e38dc15eee86fbcfdc0429727724
Paul Cadman <pwc@corefiling.com>
2015-03-17 15:09:52 +0000

    Use the highlight.js for client side code highlighting.
    Code markup must be highlighted everytime JIRA updates content on an
    issue page. So we bind the highlight action to
    JIRA.Events.NEW_CONTENT_ADDED which triggers when inline editing of a
    comment occurs (for example).
    
    We use the 'showWikiInput' JQuery event (triggered by JIRA when markup
    is rendered for a preview) to apply highlighting to previews.
    
    The former event is part of JIRA's JavaScript API [1] the latter is not, so may
    change between JIRA versions.
    
    [1] https://developer.atlassian.com/jiradev/latest-updates/developer-changes-for-older-jira-versions/preparing-for-jira-5-0/custom-fields-that-use-css-or-javascript-web-resources-in-jira-5-0
    https://jira.int.corefiling.com/browse/REVIKI-587

---
+++ src/main/resources/js/reviki-highlight.js
@@ -0,0 +1,24 @@
+function highlight() {
+  AJS.$('pre.wiki-content code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+  
+  AJS.$('code.wiki-content.inline').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+}
+
+AJS.toInit(function() {
+  // initial rendered reviki code markup on pageload
+  highlight();
+
+  // highlight rendered reviki code markup in preview
+  AJS.$(document).on("showWikiPreview", function() {
+    highlight();
+  });
+
+  // highlight rendered reviki code markup after inline edits
+  JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function(e, context, reason) {
+    highlight();
+  });
+});
